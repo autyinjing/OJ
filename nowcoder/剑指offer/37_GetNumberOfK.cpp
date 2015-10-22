c2e0bf58ed6c895a53b398268a87e0d465dbb44a
quietyin <autyinjing@126.com>
2015-10-22 23:44:06 +0800

    mod a file

--- nowcoder/剑指offer/37_GetNumberOfK.cpp
+++ nowcoder/剑指offer/37_GetNumberOfK.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int GetNumberOfK(vector<int> data ,int k) {
-        if (data.size() == 0)
-            return 0;
-        
         return count(data.begin(), data.end(), k);
     }
 };
