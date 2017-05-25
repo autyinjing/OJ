11e5c77c5b8aa49c0dbefe1e4205b8ddba6c74e7
autyinjing <autyinjing@126.com>
2017-05-25 18:14:31 +0800

    add test

--- nowcoder/PAT/1004_福尔摩斯的约会.cc
+++ nowcoder/PAT/1004_福尔摩斯的约会.cc
@@ -10,18 +10,11 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <cctype>
 
 int main(int argc, const char* argv[])
 {
-    std::map<char, std::string> str_map[2] = 
-    {
-        {{'A', "MON"},{'B', "TUE"},{'C', "WED"},{'D', "THU"},{'E', "FRI"},{'F', "SAT"},{'G', "SUN"}},
-        {   
-            {'0', "00"},{'1', "01"},{'2', "02"},{'3', "03"},{'4', "04"},{'5', "05"},{'6', "06"},{'7', "07"},{'8', "08"},{'9', "09"},{'A', "10"},{'B', "11"},
-            {'C', "12"},{'D', "13"},{'E', "14"},{'F', "15"},{'G', "16"},{'H', "17"},{'I', "18"},{'J', "19"},{'K', "20"},{'L', "21"},{'M', "22"},{'N', "23"}
-        }
-    };
-    uint32_t week_idx, hour_idx, min_idx;
+    std::string weekday[7] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
     std::string input[4], output;
     for (uint32_t i = 0; i < 4; ++i)
         std::cin >> input[i];
@@ -31,14 +24,38 @@ int main(int argc, const char* argv[])
     for (; i < input[0].size() && i < input[1].size(); ++i) {
         char h1 = input[0][i], h2 = input[1][i];
         if (h1 >= 'A' && h1 <= 'G' && h1 == h2) {
-            output = str_map[0][h1];
+            output = weekday[h1-'A'];
             break;
         }
     }
 
     //获取小时
-    for (; i < input[0].size() && i < input[1].size(); ++i) {
+    for (++i; i < input[0].size() && i < input[1].size(); ++i) {
+        char h1 = input[0][i], h2 = input[1][i];
+        if (h1 == h2) {
+            if (h1 >= '0' && h1 <= '9') {
+                output += " " + ("0" + std::to_string(h1-'0')) + ":";
+                break;
+            } else if (h1 >= 'A' && h1 <= 'N') {
+                output += " " + std::to_string(h1-'A'+10) + ":";
+                break;
+            }
+        }
     }
+
+    //获取分钟
+    for (uint32_t j = 0; j < input[2].size() && j < input[3].size(); ++j) {
+        char h1 = input[2][j], h2 = input[3][j];
+        if (isalpha(h1) && (h1 == h2)) {
+            std::string help = std::to_string(j);
+            if (help.size() == 1)
+                help = "0" + help;
+            output += help;
+            break;
+        }
+    }
+
+    std::cout << output;
 
     return 0;
 }
