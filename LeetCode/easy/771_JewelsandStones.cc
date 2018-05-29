e06c55d51b913a5c91a855941073b11fae28ab53
autyinjing <autyinjing@126.com>
2018-05-29 09:49:02 -0400

    add three question

---
+++ LeetCode/easy/771_JewelsandStones.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int numJewelsInStones(string J, string S) {
+		bool is_gem['z'+1] = {false};
+		int num = 0;
+
+		//统计宝石种类
+		for (int i = 0; i < J.size(); ++i)
+			is_gem[J[i]] = true;
+		//统计拥有宝石的数量
+		for (int i = 0; i < S.size(); ++i)
+			num += static_cast<int>(is_gem[S[i]]);
+		return num;
+	}
+};
