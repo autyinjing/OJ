2a31f1e995e87d666dd4cc1b83f84f63e0a1bdd9
autyinjing <autyinjing@126.com>
2018-06-07 06:21:49 -0400

    add question

---
+++ LeetCode/easy/463_IslandPerimeter.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	int islandPerimeter(vector<vector<int>>& grid) {
+		int h = grid.size(), l = grid[0].size(), ret = 0;
+		for (int x = 0; x < h; ++x)
+			for (int y = 0; y < l; ++y) {
+				if (grid[x][y]) {
+					//把上边和左边相邻的边一起减掉，减少了一半的减法运算
+					ret += 4;
+					if (x >= 1 && grid[x-1][y]) ret -= 2;
+					if (y >= 1 && grid[x][y-1]) ret -= 2;
+				}
+			}
+		return ret;
+	}
+};
---
+++ LeetCode/easy/669_TrimaBinarySearchTree.cc
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* trimBST(TreeNode* root, int L, int R) {
+		//二叉搜索树：中序遍历为一个有序序列，并且无重复元素
+		//1. 如果当前节点值过小，则说明当前节点及其左子树都需要减掉，直接返回右子树调整后的树
+		//2. 如果当前节点值过大，则说明当前节点及其右子树都需要减掉，直接返回左子树调整后的树
+		//3. 如果当前节点值满足条件，则需要分别调整左右子树，然后返回当前节点
+		if (!root) return nullptr;
+		if (root->val < L) return trimBST(root->right, L, R);
+		if (root->val > R) return trimBST(root->left, L, R);
+		root->left = trimBST(root->left, L, R);
+		root->right = trimBST(root->right, L, R);
+		return root;
+	}   
+};
