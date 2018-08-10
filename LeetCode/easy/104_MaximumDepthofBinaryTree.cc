be880ef05f1cf122368292f88fee20c8e029cfbf
autyin <autyin@123u.com>
2018-08-10 15:04:46 +0800

    替换tab为空格

--- LeetCode/easy/104_MaximumDepthofBinaryTree.cc
+++ LeetCode/easy/104_MaximumDepthofBinaryTree.cc
@@ -9,13 +9,13 @@
  */
 class Solution {
 public:
-	int maxDepth(TreeNode* root) {
-		//std::max()很慢？
-		//return root ? (max(maxDepth(root->left), maxDepth(root->right)) + 1) : 0;
-		if (!root)
-			return 0;
-		int left_depth = maxDepth(root->left);
-		int right_depth = maxDepth(root->right);
-		return (left_depth > right_depth ? left_depth : right_depth) + 1;
-	}
+    int maxDepth(TreeNode* root) {
+        //std::max()很慢？
+        //return root ? (max(maxDepth(root->left), maxDepth(root->right)) + 1) : 0;
+        if (!root)
+            return 0;
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+        return (left_depth > right_depth ? left_depth : right_depth) + 1;
+    }
 };
